f5db78abc74c52fc0f7ff128440bc2c4955dae4e
MissDine <enidnyatichi1212@gmail.com>
2022-05-06 09:36:01 +0300

    Complted assignment

--- src/components/ChangeItUp.js
+++ src/components/ChangeItUp.js
@@ -11,7 +11,7 @@ function ChangeItUp() {
         type="text"
         name="search"
         onChange={handleChange}
-        placeholder="Enter search term..."
+        placeholder="Enter search name..."
       />
 
       <select name="filter" onChange={handleChange}>
